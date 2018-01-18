2c6a82520827deb7d16baebfeab372700592a9b0
nat-nat33 <nramirez787@gmail.com>
2018-01-17 21:41:01 -1000

    adding description of data

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+from matplotlib import pyplot as plt
+%matplotlib inline
 
 #Reading the dataset in a dataframe using Pandas
-df = pd.read_csv("data/mock_app_data.csv") 
+df = pd.read_csv("./data/mock_app_data.csv") 
 
+#returns number of rows
+print("num of rows: ", len(df.index))
+
+#returns number of columns
+print("num of columns: ", len(df. columns))
+
+#returns first 10 rows with header
 df.head(10)
+
